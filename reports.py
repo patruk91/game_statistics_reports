75edc4e39633ffd58b7d3ddf670332d815b66aee
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-01-22 06:58:49 +0100

    Display if game exists in database

--- reports.py
+++ reports.py
@@ -4,7 +4,7 @@ file_name = "game_stat.txt"
 def count_games(file_name):
     """
     Calculate how many games are in the file.
-    :param file_name: Text file where included informations about games.
+    :param file_name: Text file where include information about games.
     :return: Total amount games in file_name.
     """
     with open(file_name) as file_object:
@@ -12,4 +12,15 @@ def count_games(file_name):
     return len(games)
 
 
-
+def decide(file_name, year):
+    """
+    Display if there is game from a given year.
+    :param file_name: Text file where include information about games.
+    :param year: Game release year.
+    :return: Boolean value: True if game exists, or False if not.
+    """
+    with open(file_name) as file_object:
+        for line in file_object:
+            if str(year) in line:
+                return True
+    return False
