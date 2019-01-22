75d4fba39e3365dafc6d8ae2685d40973759e309
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-01-22 07:01:05 +0100

    Return the title of newest game

--- reports.py
+++ reports.py
@@ -24,3 +24,17 @@ def decide(file_name, year):
             if str(year) in line:
                 return True
     return False
+
+
+def get_latest(file_name):
+    """
+    Display the newest title of game.
+    :param file_name: Text file where include information about games.
+    :return: Title of the game.
+    """
+    with open(file_name) as file_object:
+        games = file_object.readlines()
+    years = [int(game.rstrip().split("\t")[2]) for game in games]
+    indices = years.index(max(years))
+
+    return games[indices].split("\t")[0]
