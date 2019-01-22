c1b91e1f4d2f8cfe4e17a61e21b3c5ebebf99cbd
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-01-22 06:40:52 +0100

    Count how many games are in the text file

--- reports.py
+++ reports.py
@@ -1,2 +1,15 @@
+file_name = "game_stat.txt"
 
-# Report functions
+
+def count_games(file_name):
+    """
+    Calculate how many games are in the file.
+    :param file_name: Text file where included informations about games.
+    :return: Total amount games in file_name.
+    """
+    with open(file_name) as file_object:
+        games = file_object.readlines()
+    return len(games)
+
+
+
