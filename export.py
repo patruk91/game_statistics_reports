fc5d82f1dd365e0b48c2cdd8b9e7eed338a62491
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-01-24 07:12:19 +0100

    Add docstrings to export functions

--- export.py
+++ export.py
@@ -5,14 +5,17 @@ import reports
 
 
 filename = "game_stat.txt"
+exported_file = "exported_answers.txt"
 
 
 def export_to_file(data_from_user):
-    with open("exported_answers.txt", "a") as file_object:
+    """Export data from other functions to a file"""
+    with open(exported_file, "a") as file_object:
         file_object.write(data_from_user)
 
 
 def export_count_games():
+    """Export to file: how many games are in the file."""
     numbers_of_games = str(reports.count_games(filename)) + "\n"
     message = "Total games in {}: {}".format(filename, numbers_of_games)
 
@@ -21,6 +24,7 @@ def export_count_games():
 
 
 def export_decide():
+    """Export to file: if there is game from a given year."""
     year = int(input("Enter the searched year: "))
     value = reports.decide(filename, year)
     if value is True:
@@ -36,6 +40,7 @@ def export_decide():
 
 
 def export_get_latest():
+    """Export to file: the newest title of game."""
     latest_game = reports.get_latest(filename)
     message = "Title of latest game: {}" .format(latest_game)
 
@@ -44,6 +49,7 @@ def export_get_latest():
 
 
 def export_count_by_genre():
+    """Export to file: how many games is by genre."""
     genre = input("Please. give me a genre of game: ")
     genres = reports.count_by_genre(filename, genre)
     message = "We have {} games from {} genre." .format(genres, genre)
@@ -53,6 +59,7 @@ def export_count_by_genre():
 
 
 def export_get_line_number_by_title():
+    """Export to file: the line number of the given game (by title)."""
     title = input("Please provide title of the game: ")
     line_number = reports.get_line_number_by_title(filename, title)
     message = "The number of line in {} is {}" .format(filename, line_number)
@@ -62,6 +69,7 @@ def export_get_line_number_by_title():
 
 
 def export_sort_abc():
+    """Export to file: titles of games in alphabetical order."""
     sorted_games = reports.sort_abc(filename)
     print("List of games in alphabetical order: ")
     for game_title in sorted_games:
@@ -71,6 +79,7 @@ def export_sort_abc():
 
 
 def export_get_genres():
+    """Export to file: a list of all genres without duplicates."""
     game_genres = reports.get_genres(filename)
     for genre in game_genres:
         export_to_file(genre + "\n")
@@ -79,6 +88,7 @@ def export_get_genres():
 
 
 def export_when_was_top_sold_fps():
+    """Export to file: release date of the top sold "First-person shooter"."""
     genre = input("Please, give me a searched genre game: ")
     top_game = reports.when_was_top_sold_fps(filename, genre)
     message = "The top sold {} genre was released in: {}" .format(genre, top_game)
@@ -88,15 +98,19 @@ def export_when_was_top_sold_fps():
 
 
 def choose_option():
-    print("1. how many games are in the file.")
-    print("2. is there a game from a given year.")
-    print("3. the latest game.")
-    print("4. how many games do we have by genre.")
-    print("5. line number of the given game (by title)")
-    print("6. alphabetical ordered list of the titles")
-    print("7. all genres without duplicates")
-    print("8. release date of the top sold game (by genre)")
-    print("9. Exit")
+    """
+    Display menu with options.
+    :return: user option
+    """
+    print("1. how many games are in the file."
+          "\n2. is there a game from a given year."
+          "\n3. the latest game."
+          "\n4. how many games do we have by genre."
+          "\n5. line number of the given game (by title)"
+          "\n6. alphabetical ordered list of the titles"
+          "\n7. all genres without duplicates"
+          "\n8. release date of the top sold game (by genre)"
+          "\n9. Exit")
 
     option = input("\nExport: ")
     return option
