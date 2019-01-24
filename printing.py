bcb3efc030c9c40bd40322932833beb6cf882e01
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-01-24 06:37:02 +0100

    Print functions add

--- printing.py
+++ printing.py
@@ -1,2 +1,102 @@
+import os
 
-# Printing functions
+
+import reports
+
+
+filename = "game_stat.txt"
+
+
+def display_count_games():
+    numbers_of_games = reports.count_games(filename)
+    print("Total games in {}: {}\n" .format(filename, numbers_of_games))
+
+
+def display_decide():
+    year = int(input("Enter the searched year: "))
+    value = reports.decide(filename, year)
+    if value is True:
+        print("We have a game from {} year in {}\n" .format(str(year), filename))
+    else:
+        print("Sorry, we don't have game from {} year in {}\n".format(str(year), filename))
+
+
+def display_get_latest():
+    latest_game = reports.get_latest(filename)
+    print("Title of latest game: {}\n" .format(latest_game))
+
+
+def display_count_by_genre():
+    genre = input("Please. give me a genre of game: ")
+    genres = reports.count_by_genre(filename, genre)
+    print("We have {} games from {} genre.\n" .format(genres, genre))
+
+
+def display_get_line_number_by_title():
+    title = input("Please provide title of the game: ")
+    line_number = reports.get_line_number_by_title(filename, title)
+    print("The number of line in {} is {}\n" .format(filename, line_number))
+
+
+def display_sort_abc():
+    sorted_games = reports.sort_abc(filename)
+    print("List of games in alphabetical order: ")
+    [print(game_title) for game_title in sorted_games]
+    print()
+
+
+def display_get_genres():
+    game_genres = reports.get_genres(filename)
+    [print(genre) for genre in game_genres]
+    print()
+
+
+def display_when_was_top_sold_fps():
+    genre = input("Please, give me a searched genre game: ")
+    top_game = reports.when_was_top_sold_fps(filename, genre)
+    print("The top sold {} genre was released in: {}\n" .format(genre, top_game))
+
+
+def choose_option():
+    print("1. how many games are in the file.")
+    print("2. is there a game from a given year.")
+    print("3. the latest game.")
+    print("4. how many games do we have by genre.")
+    print("5. line number of the given game (by title)")
+    print("6. alphabetical ordered list of the titles")
+    print("7. all genres without duplicates")
+    print("8. release date of the top sold game (by genre)")
+    print("9. Exit")
+
+    option = input("\nDisplay: ")
+    return option
+
+
+def main():
+    os.system('clear')
+    while True:
+        option = choose_option()
+        os.system('clear')
+
+        if option == "1":
+            display_count_games()
+        elif option == "2":
+            display_decide()
+        elif option == "3":
+            display_get_latest()
+        elif option == "4":
+            display_count_by_genre()
+        elif option == "5":
+            display_get_line_number_by_title()
+        elif option == "6":
+            display_sort_abc()
+        elif option == "7":
+            display_get_genres()
+        elif option == "8":
+            display_when_was_top_sold_fps()
+        elif option == "9":
+            break
+
+
+if __name__ == "__main__":
+    main()
--- reports.py
+++ reports.py
@@ -1,89 +1,87 @@
-file_name = "game_stat.txt"
-
-
-def read_data_from_file(file_name):
+def read_data_from_file(filename):
     """
     Read data about games from indicated file.
-    :param file_name: Text file where include information about games.
+    :param filename: Text file where include information about games.
     :return: List of games
     """
-    with open(file_name) as file_object:
+    with open(filename) as file_object:
         games = file_object.readlines()
     return games
 
 
-def count_games(file_name):
+def count_games(filename):
     """
     Calculate how many games are in the file.
-    :param file_name: Text file where include information about games.
-    :return: Total amount games in file_name.
+    :param filename: Text file where include information about games.
+    :return: Total amount games in filename.
     """
-    games = read_data_from_file(file_name)
+    games = read_data_from_file(filename)
     return len(games)
 
 
-def decide(file_name, year):
+def decide(filename, year):
     """
     Return if there is game from a given year.
-    :param file_name: Text file where include information about games.
+    :param filename: Text file where include information about games.
     :param year: Game release year.
     :return: Boolean value: True if game exists, or False if not.
     """
-    with open(file_name) as file_object:
-        for line in file_object:
-            if str(year) in line:
-                return True
+    games = read_data_from_file(filename)
+
+    years = [int(game.rstrip().split("\t")[2]) for game in games]
+    if year in years:
+        return True
     return False
 
 
-def get_latest(file_name):
+def get_latest(filename):
     """
     Return the newest title of game.
-    :param file_name: Text file where include information about games.
+    :param filename: Text file where include information about games.
     :return: Title of the game.
     """
-    games = read_data_from_file(file_name)
+    games = read_data_from_file(filename)
 
     years = [int(game.rstrip().split("\t")[2]) for game in games]
     indices = years.index(max(years))
     return games[indices].split("\t")[0]
 
 
-def count_by_genre(file_name, genre):
+def count_by_genre(filename, genre):
     """
     Calculate how many games is by genre.
-    :param file_name: Text file where include information about games.
+    :param filename: Text file where include information about games.
     :param genre: Type/genre of the game.
     :return: Amount of games, searched by genre.
     """
-    games = read_data_from_file(file_name)
+    games = read_data_from_file(filename)
 
     genres = [game.rstrip().split("\t")[3] for game in games]
     return genres.count(genre)
 
 
-def get_line_number_by_title(file_name, title):
+def get_line_number_by_title(filename, title):
     """
     Find the line number of the given game (by title).
-    :param file_name: Text file where include information about games.
+    :param filename: Text file where include information about games.
     :param title: Title of the game.
     :return: Line number from the file.
     """
-    games = read_data_from_file(file_name)
+    games = read_data_from_file(filename)
     line_number = [(index + 1) for index, game in enumerate(games) if title in game]
     if len(line_number) != 0:
         return line_number[0]
     else:
-        raise ValueError("Could not find {} in {}".format(title, file_name))
+        raise ValueError("Could not find {} in {}".format(title, filename))
 
 
-def sort_abc(file_name):
+def sort_abc(filename):
     """
     Return titles of games in alphabetical order.
-    :param file_name: Text file where include information about games.
+    :param filename: Text file where include information about games.
     :return: Sorted game titles.
     """
-    games = read_data_from_file(file_name)
+    games = read_data_from_file(filename)
 
     games_titles = [game.rstrip().split("\t")[0] for game in games]
     i = 0
@@ -97,13 +95,13 @@ def sort_abc(file_name):
     return games_titles
 
 
-def get_genres(file_name):
+def get_genres(filename):
     """
     Return a list of all genres without duplicates.
-    :param file_name: Text file where include information about games.
+    :param filename: Text file where include information about games.
     :return: list of genres
     """
-    games = read_data_from_file(file_name)
+    games = read_data_from_file(filename)
 
     games_genres = set([game.rstrip().split("\t")[3] for game in games])
     games_genres = list(sorted(games_genres))
@@ -111,20 +109,20 @@ def get_genres(file_name):
     return games_genres
 
 
-def when_was_top_sold_fps(file_name, genre="First-person shooter"):
+def when_was_top_sold_fps(filename, genre="First-person shooter"):
     """
     Find release date of the top sold "First-person shooter".
-    :param file_name: Text file where include information about games.
+    :param filename: Text file where include information about games.
     :param genre: String of searched genre.
     :return: year of top sold game
     """
-    games = read_data_from_file(file_name)
+    games = read_data_from_file(filename)
 
     games_shooters = [game.rstrip().split("\t") for game in games if genre in game]
     sold_copies = [(float(shooter[1])) for shooter in games_shooters]
     if len(sold_copies) != 0:
         index_top_sold_game = sold_copies.index(max(sold_copies))
     else:
-        raise ValueError("Could not find {} in {}".format(genre, file_name))
+        raise ValueError("Could not find {} in {}".format(genre, filename))
 
     return int(games_shooters[index_top_sold_game][2])
